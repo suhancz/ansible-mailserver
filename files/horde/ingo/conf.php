889829285a3660cd475013333ccce57845d87c02
Akos Balla <akos.balla@sirc.hu>
2022-03-13 01:47:20 +0100

    configure all Horde

---
+++ files/horde/ingo/conf.php
@@ -0,0 +1,10 @@
+<?php
+/* CONFIG START. DO NOT CHANGE ANYTHING IN OR AFTER THIS LINE. */
+// $Hash: 250aa3932bf63e25d99fe1399ded4932870fa93f
+// $Id: 48142d13ef06c07f56427fe5b43981631bdbfdb0 $
+$conf['storage']['params']['driverconfig'] = 'horde';
+$conf['storage']['driver'] = 'sql';
+$conf['rules']['userheader'] = true;
+$conf['spam']['header'] = 'X-Spam-Level';
+$conf['spam']['compare'] = 'numeric';
+/* CONFIG END. DO NOT CHANGE ANYTHING IN OR BEFORE THIS LINE. */
---
+++ files/horde/mnemo/conf.php
@@ -0,0 +1,10 @@
+<?php
+/* CONFIG START. DO NOT CHANGE ANYTHING IN OR AFTER THIS LINE. */
+// $Hash: 31297e40af221ce5b4960a5db551d8559568df6f
+// $Id: d97e56b407852ff0a86c7d88c9a57c8f3089e82f $
+$conf['storage']['params']['table'] = 'mnemo_memos';
+$conf['storage']['params']['driverconfig'] = 'horde';
+$conf['storage']['driver'] = 'sql';
+$conf['notepads']['driver'] = 'default';
+$conf['menu']['import_export'] = true;
+/* CONFIG END. DO NOT CHANGE ANYTHING IN OR BEFORE THIS LINE. */
