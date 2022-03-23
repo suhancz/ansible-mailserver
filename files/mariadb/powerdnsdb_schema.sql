73450ad9ca75a1404570c35cb7c7c147cc9499c9
Akos Balla <akos.balla@sirc.hu>
2022-03-23 02:18:57 +0100

    use schema from package

--- files/mariadb/powerdnsdb_schema.sql
+++ files/mariadb/powerdnsdb_schema.sql
@@ -1,92 +1,161 @@
-use powerdnsdb;
+-- MySQL dump 10.19  Distrib 10.3.28-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: powerdnsdb
+-- ------------------------------------------------------
+-- Server version	10.3.28-MariaDB
 
-CREATE TABLE domains (
-  id INT AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  master VARCHAR(128) DEFAULT NULL,
-  last_check INT DEFAULT NULL,
-  type VARCHAR(6) NOT NULL,
-  notified_serial INT DEFAULT NULL,
-  account VARCHAR(40) DEFAULT NULL,
-  PRIMARY KEY (id)
-) Engine=InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE UNIQUE INDEX name_index ON domains(name);
+--
+-- Table structure for table `comments`
+--
 
-CREATE TABLE records (
-  id BIGINT AUTO_INCREMENT,
-  domain_id INT DEFAULT NULL,
-  name VARCHAR(255) DEFAULT NULL,
-  type VARCHAR(10) DEFAULT NULL,
-  content VARCHAR(64000) DEFAULT NULL,
-  ttl INT DEFAULT NULL,
-  prio INT DEFAULT NULL,
-  change_date INT DEFAULT NULL,
-  disabled TINYINT(1) DEFAULT 0,
-  ordername VARCHAR(255) BINARY DEFAULT NULL,
-  auth TINYINT(1) DEFAULT 1,
-  PRIMARY KEY (id)
-) Engine=InnoDB;
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `modified_at` int(11) NOT NULL,
+  `account` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
+  `comment` text CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comments_name_type_idx` (`name`,`type`),
+  KEY `comments_order_idx` (`domain_id`,`modified_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
---CREATE INDEX name_index ON domains(name);
+--
+-- Table structure for table `cryptokeys`
+--
 
-CREATE INDEX nametype_index ON records(name,type);
+DROP TABLE IF EXISTS `cryptokeys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cryptokeys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain_id` int(11) NOT NULL,
+  `flags` int(11) NOT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `published` tinyint(1) DEFAULT 1,
+  `content` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `domainidindex` (`domain_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE INDEX domain_id ON records(domain_id);
+--
+-- Table structure for table `domainmetadata`
+--
 
-CREATE INDEX recordorder ON records (domain_id, ordername);
+DROP TABLE IF EXISTS `domainmetadata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domainmetadata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain_id` int(11) NOT NULL,
+  `kind` varchar(32) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `domainmetadata_idx` (`domain_id`,`kind`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE supermasters (
-  ip VARCHAR(64) NOT NULL,
-  nameserver VARCHAR(255) NOT NULL,
-  account VARCHAR(40) NOT NULL,
-  PRIMARY KEY (ip, nameserver)
-) Engine=InnoDB;
+--
+-- Table structure for table `domains`
+--
 
-CREATE TABLE comments (
-  id INT AUTO_INCREMENT,
-  domain_id INT NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  type VARCHAR(10) NOT NULL,
-  modified_at INT NOT NULL,
-  account VARCHAR(40) NOT NULL,
-  comment VARCHAR(64000) NOT NULL,
-  PRIMARY KEY (id)
-) Engine=InnoDB;
+DROP TABLE IF EXISTS `domains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domains` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `master` varchar(128) DEFAULT NULL,
+  `last_check` int(11) DEFAULT NULL,
+  `type` varchar(6) NOT NULL,
+  `notified_serial` int(10) unsigned DEFAULT NULL,
+  `account` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_index` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE INDEX comments_domain_id_idx ON comments (domain_id);
+--
+-- Table structure for table `records`
+--
 
-CREATE INDEX comments_name_type_idx ON comments (name, type);
+DROP TABLE IF EXISTS `records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `records` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `domain_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `content` varchar(64000) DEFAULT NULL,
+  `ttl` int(11) DEFAULT NULL,
+  `prio` int(11) DEFAULT NULL,
+  `disabled` tinyint(1) DEFAULT 0,
+  `ordername` varchar(255) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
+  `auth` tinyint(1) DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `nametype_index` (`name`,`type`),
+  KEY `domain_id` (`domain_id`),
+  KEY `ordername` (`ordername`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE INDEX comments_order_idx ON comments (domain_id, modified_at);
+--
+-- Table structure for table `supermasters`
+--
 
-CREATE TABLE domainmetadata (
-  id INT AUTO_INCREMENT,
-  domain_id INT NOT NULL,
-  kind VARCHAR(32),
-  content TEXT,
-  PRIMARY KEY (id)
-) Engine=InnoDB;
+DROP TABLE IF EXISTS `supermasters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `supermasters` (
+  `ip` varchar(64) NOT NULL,
+  `nameserver` varchar(255) NOT NULL,
+  `account` varchar(40) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`ip`,`nameserver`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE INDEX domainmetadata_idx ON domainmetadata (domain_id, kind);
+--
+-- Table structure for table `tsigkeys`
+--
 
-CREATE TABLE cryptokeys (
-  id INT AUTO_INCREMENT,
-  domain_id INT NOT NULL,
-  flags INT NOT NULL,
-  active BOOL,
-  content TEXT,
-  PRIMARY KEY(id)
-) Engine=InnoDB;
+DROP TABLE IF EXISTS `tsigkeys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tsigkeys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `algorithm` varchar(50) DEFAULT NULL,
+  `secret` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `namealgoindex` (`name`,`algorithm`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-CREATE INDEX domainidindex ON cryptokeys(domain_id);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-CREATE TABLE tsigkeys (
-  id INT AUTO_INCREMENT,
-  name VARCHAR(255),
-  algorithm VARCHAR(50),
-  secret VARCHAR(255),
-  PRIMARY KEY (id)
-) Engine=InnoDB;
-
-CREATE UNIQUE INDEX namealgoindex ON tsigkeys(name, algorithm);
+-- Dump completed on 2022-03-23  2:16:45
